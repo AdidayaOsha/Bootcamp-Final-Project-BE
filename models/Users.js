feacd5395cadafbb4fbe4a3d79fed2704723067c
ADIP1598 <dhiyailmamputra@gmail.com>
2022-04-07 01:15:21 +0700

    Nambah dikit
    Nambah dikit

--- models/Users.js
+++ models/Users.js
@@ -39,7 +39,7 @@ const Users = sequelize.define("users", {
     type: DataTypes.INTEGER,
     allowNull: true,
   },
-  is_verified: {
+  is_verifiedd: {
     type: DataTypes.BOOLEAN,
     allowNull: true,
   },
