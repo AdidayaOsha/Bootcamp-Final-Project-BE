212c86b02fcce5499f448491c5fc9d07c5be6c50
AdidayaOsha <adidayaosha@gmail.com>
2022-04-18 20:44:19 +0700

    join version

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,7 +1,20 @@
 const db = require("../db");
+const Carts = require("../models/Carts");
+const Products = require("../models/Products");
+const Users = require("../models/Users");
 
 module.exports = {
-  getUserCart: (req, res) => {},
+  getCart: async (req, res) => {
+    // Products.sync({ alter: true });
+    try {
+      let carts = await Carts.findAll({
+        include: [{ model: Products }, { model: Users }],
+      });
+      res.status(200).send(carts);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  },
   updateUserCart: (req, res) => {},
   addUserCart: (req, res) => {},
   deleteUserCart: (req, res) => {},
