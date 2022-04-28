b208ce47ce6f971430a14e9e7400c8d07ea8a95f
AdidayaOsha <adidayaosha@gmail.com>
2022-04-28 19:59:46 +0700

    added Change Quantity Feature for Cart

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { cartController } = require("../controllers");
 
 router.get("/get/:id", cartController.getUserCart);
-router.patch("/update/:id", cartController.updateUserCart);
+router.patch("/quantity/:id", cartController.updateCartQty);
 router.post("/add", cartController.addUserCart);
 router.delete("/delete/:id", cartController.deleteUserCart);
 
