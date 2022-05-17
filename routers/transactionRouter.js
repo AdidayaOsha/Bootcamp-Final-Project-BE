170a88ffc4a7cd53c41a5f0f6708e75f75fd723c
ADIP1598 <dhiyailmamputra@gmail.com>
2022-05-17 21:42:00 +0700

    Add Sort by Time

--- routers/transactionRouter.js
+++ routers/transactionRouter.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const { transactionController } = require("../controllers");
 
 router.get("/", transactionController.getTransaction);
+router.get("/asc", transactionController.getTransactionSortAsc);
+router.get("/desc", transactionController.getTransactionSortDesc);
 router.get("/:id", transactionController.getTransactionById);
 router.post("/:id", transactionController.checkStock);
 router.patch("/:id", transactionController.changeTransactionStatus);
